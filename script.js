ef87163974cce49063615e3a496047200d0c20a4
Am706870 <88397078+Am706870@users.noreply.github.com>
2025-02-21 12:31:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+// Function to find the DOM level of the element with id="level"
+function levelFinder() {
+	var targetElement = document.getElementById('level');
+	var level = 0;
+	while(targetElement){
+		level++;
+		targetElement = targetElement.parentElement;	
+	}
+	alert(`The level of the element is:` +level);
+
+}
+
+// Call the function when the DOM is fully loaded
+window.onload = levelFinder; 
+
+		
